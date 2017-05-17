a899f32c4f617bd065ffa3f8348ef33e8647f985
Jack Pincus <jwpincus@gmail.com>
2017-05-17 14:59:24 -0600

    change config to avoid sprockets

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.enabled = false
 module Puntstartr
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
