cd104617e3e322c2a3715455e9461a30a663bc1f
Cody <codyborders@gmail.com>
2017-05-17 17:25:12 -0600

    Adds additional expectations for navbar Adds links to about us, categories, log in, and sign up Moves logo to center

--- spec/features/user_views_navbar_spec.rb
+++ spec/features/user_views_navbar_spec.rb
@@ -6,7 +6,10 @@ RSpec.feature "User views navbar" do
     visit "/"
 
     within("div.nav-wrapper") do
-      expect(page).to have_content("Sign In")
+      expect(page).to have_content("Log In")
+			expect(page).to have_content("Sign Up")
+			expect(page).to have_content("Categories")
+			expect(page).to have_content("About Us")
     end
   end
 end
