50aaf0f6d03845decb76a9e69ed794d1fb3a0030
stovermc <stovermc@gmail.com>
2017-05-18 16:38:23 -0600

    add authentication module

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -5,5 +5,6 @@ module AuthenticationHelper
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     fill_in "Password confirmation", with: user.password
+    
   end
 end
