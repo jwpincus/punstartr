178d9b867399e40d2f205bdd068b9b4c2151984c
Jack Pincus <jwpincus@gmail.com>
2017-05-18 11:55:11 -0600

    update seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518164740) do
+ActiveRecord::Schema.define(version: 20170518173617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20170518164740) do
     t.decimal  "target_amount"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_projects_on_category_id", using: :btree
   end
 
+  add_foreign_key "projects", "categories"
 end
