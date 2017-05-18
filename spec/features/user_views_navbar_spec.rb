d0e80659c945cd8eca3f4dc31acc02996030ffca
stovermc <stovermc@gmail.com>
2017-05-18 16:21:56 -0600

    Add authentication helper

--- spec/features/user_views_navbar_spec.rb
+++ spec/features/user_views_navbar_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "User views navbar" do
   scenario "guest user can view and use navbar" do
-    
+
     visit "/"
 
     within("div.nav-wrapper") do
@@ -12,5 +12,10 @@ RSpec.feature "User views navbar" do
 			expect(page).to have_content("About Us")
 			expect(page).to have_content("Start a Project")
     end
+  end
+
+  scenario "logged in user see's name in navbar" do
+    
+
   end
 end
