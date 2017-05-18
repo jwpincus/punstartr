505c774fbb52d8529320b23de872ab8c3b5fe98b
Jack Pincus <jwpincus@gmail.com>
2017-05-18 11:30:48 -0600

    add filter by tool to categories index

--- spec/features/categories_index_spec.rb
+++ spec/features/categories_index_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe "As a user" do
   context "When I visit the root path and click categories in Navbar" do
-    it "I expect to be taken to the categories index" do
+    it "I expect to be taken to the categories index and see navbar" do
       visit '/'
       click_on "Categories"
       expect(current_path).to eq('/categories')
-      expect
+      expect(page).to have_css("div.nav-wrapper")
+    end
+    it 'I expect to see a list of category names' do
+      category_1, category_2 = create_list(:category, 2)
+      visit '/categories'
+      expect(page).to have_select('category', options: [category_1.name, category_2.name])
     end
   end
 end
