472db112df61e711ec7048f2606a73f1f16a6ca9
Josh Thompson <thompsonjoshd@gmail.com>
2017-05-20 12:46:47 -0600

    basic user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "should validate presence of name, email" do
+      it { should validate_presence_of(:name) }
+      it { should validate_presence_of(:email) }
+    end
+  end
+
+
 end
