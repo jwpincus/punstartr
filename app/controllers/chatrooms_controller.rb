a70ffc719affc5214b26138823229c0d187253e1
stovermc <stovermc@gmail.com>
2017-05-24 10:54:21 -0600

    add controllers and forms
    * message and chatroom controllers
    
    * forms for new messages and show action for chatroom

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,6 @@
+class ChatroomsController < ApplcationController
+  def show
+    @chatroom = Chatroom.find_by(slug: params[:slug])
+    @message = Message.new
+  end
+end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,5 +1,5 @@
 class Chatroom < ActiveRecord
-  has_many :messages
+  has_many :messages, dependent: :destroy
   has_many :users, through: :messages
   validates :topic, presence: true, uniqueness: true, case_sensitive: false
 end
