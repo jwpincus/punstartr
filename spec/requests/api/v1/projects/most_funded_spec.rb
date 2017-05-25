9ce96a8e0224b8df0e24907547db61d5031ad5e5
Cody Borders <codyborders@gmail.com>
2017-05-24 23:24:17 -0600

    Fixes line spacing and removes commented out story
    I don't think this test is optimal but it's passing and the functionality looks fine when I run the server so I'm going with it for now.

--- spec/requests/api/v1/projects/most_funded_spec.rb
+++ spec/requests/api/v1/projects/most_funded_spec.rb
@@ -21,17 +21,3 @@ describe "Most funded projects" do
   end
 end
 
-# As a user
-# When I visit "/api/v1/projects/most_funded
-# I receive valid JSON
-# And it is a status 200
-# And it is formatted as:
-# [{"title"=>"project name",
-#  "description"=>
-#   "project description",
-#  "image_url"=>"project image"}
-
-# I expect to see a list of the ten projects with the greatest amount of funding in dollars
-
-
-# I expect each project name to be a link to that project's show page
