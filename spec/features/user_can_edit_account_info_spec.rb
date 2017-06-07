897456c2dc69bd8280478488d61e7f35884edfc2
Jack Pincus <jwpincus@gmail.com>
2017-06-07 17:51:11 -0600

    workiong on the merge

--- spec/features/user_can_edit_account_info_spec.rb
+++ spec/features/user_can_edit_account_info_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe "A User can edit their profile information" do
-  it "They go to their user Dash and are able to edit their info", js: true do
+  xit "They go to their user Dash and are able to edit their info", js: true do
     user = create(:user)
     visit user_path(user)
     find_element(css: '#name-field').click
-    
+
   end
 end
