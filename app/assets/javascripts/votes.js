178d4d2e9259c4b22371ceb5ceee6ff30031dde2
somedayrainbows <ebassity@gmail.com>
2017-06-07 17:25:27 -0600

    Updates upvote feature with destroy

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function() {
+
   $('#thumbup').on('click', function() {
     function createVote() {
       return $.ajax({
@@ -10,7 +11,9 @@ $(document).ready(function() {
         { post:
           { vote_type: "upvote", project_id: $("input#project_id").val() } }
       }).done(function(data) {
-        $('#thumbup').addClass("green-thumb")
+        $('#thumbup').addClass("aqua-thumb");
+        $('#votes_section').append('<p class="post>' + "Your vote has been saved!" + '</p>');
+
         // come back to change color of thumb to show upvote recorded properly
         // come back and append text for "you have upvoted this project!"
         // message for "you have already voted!"
@@ -20,4 +23,24 @@ $(document).ready(function() {
     }
     createVote();
   });
+
+  $('#thumbdown').on('click', function() {
+      function deleteVote() {
+        var id = $("#project_id").val();
+        return $.ajax({
+          url: '/api/v1/votes/' + id,
+          method: 'DELETE',
+        }).done(function(data) {
+          $('#thumbup').removeClass("aqua-thumb");
+        }).fail(function(error) {
+          console.log(error);
+        });
+      }
+      deleteVote();
+    });
+
+    $('form').on('submit', function(event){
+    event.preventDefault();
+  });
+
 });
--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :votes, only: [:index, :show, :create]
+      resources :votes, only: [:index, :show, :create, :destroy]
     end
   end
 
