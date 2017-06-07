9f6a5057badd55a6b2fe5feb6b2711aae6eed41e
NessEFC <NessEFC@gmail.com>
2017-06-07 17:11:24 -0600

    pass the create comment spec using selenium.

--- spec/features/user_can_add_comments_spec.rb
+++ spec/features/user_can_add_comments_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "As a logged-in user" do
+RSpec.feature "As a logged-in user", js: true do
   scenario "they can add a comment to a project" do
-
-    skip
 
     project = create(:project)
     user = create(:user)
@@ -17,26 +15,19 @@ RSpec.feature "As a logged-in user" do
 
     expect(current_path).to eq(project_path(project))
     expect(page).to have_content('Comments')
-
-    within('#comments-list') do
-      expect(page).to have_content('')
-    end
+    expect('#comments-list').to have_content('')
 
     fill_in 'comment[body]', with: 'This project is rad.'
 
     click_on 'Submit Comment'
 
-    visit '/'
-    first('.card-content').click_on(project.title)
-    expect(current_path).to eq(project_path(project))
+    sleep(5)
 
     comment = Comment.last
 
     expect(comment.body).to eq('This project is rad.')
-
-    within('.comments-list') do
+    within('#comments-list') do
       expect(page).to have_content(comment.body)
     end
-
   end
 end
