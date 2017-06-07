168a40dd042efa7798d2ae3c0d736133562cb753
Jack Pincus <jwpincus@gmail.com>
2017-06-07 13:22:12 -0600

    pre-merge commit

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,9 @@ Rails.application.routes.draw do
     post :authorization, on: :collection
   end
   resources :payments, only: [:create]
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
---
+++ spec/features/user_can_edit_account_info_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe "A User can edit their profile information" do
+  it "They go to the User Dash and are able to go to edit screen" do
+    
+  end
+end
