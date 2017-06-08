006827182a9875ed1bd25f9293ed636feb34b230
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-06-08 01:30:30 -0600

    email changes

--- config/application.rb
+++ config/application.rb
@@ -24,3 +24,21 @@ module Puntstartr
   end
 end
 # config.assets.initialize_on_precompile = false
+
+module AddCollaborator
+  class Application < Rails::Application
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.sendgrid.net',
+      port:                 '587',
+      domain:               'example.com',
+      user_name:            ENV["SENDGRID_USERNAME"],
+      password:             ENV["SENDGRID_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
