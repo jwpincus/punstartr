9b17f29ba53f7c450e29e42b775968297ef85b3c
Jack Pincus <jwpincus@gmail.com>
2017-06-07 19:19:01 -0600

    add test for user edit story

--- spec/features/user_can_edit_account_info_spec.rb
+++ spec/features/user_can_edit_account_info_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 describe "A User can edit their profile information" do
-  xit "They go to their user Dash and are able to edit their info", js: true do
+  it "They go to their user Dash and are able to edit their info", js: true do
     user = create(:user)
+    allow_any_instance_of(ApplicationController)
+    .to receive(:current_user)
+    .and_return(user)
     visit user_path(user)
-    find_element(css: '#name-field').click
-
+    element = find('#name-field')
+    page.find('#name-field').click
+    find('#name-field').base.send_keys('Jack')
+    visit user_path(user)
+    # expect(page).to have_content('Jack')
   end
 end
